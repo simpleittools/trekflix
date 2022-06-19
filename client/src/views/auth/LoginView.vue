7814b9b7d21410aa3ea76dc1e81753bcef330c99
icedvigilante <wm.ryan.mooney@gmail.com>
2022-06-18 16:48:31 -0800

    setup login with validation and components

--- client/src/views/auth/LoginView.vue
+++ client/src/views/auth/LoginView.vue
@@ -8,25 +8,15 @@
 
     <div class="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
       <div class="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
-        <form class="space-y-6" @submit.prevent="handleSubmit">
-          <div class="mt-1 sm:mt-0 sm:col-span-2">
-            <div class="input-frame">
-              <span class="input-label"> Username </span>
-              <input type="text" v-model="data.username" id="username" autocomplete="username" class="input-field" />
-            </div>
-          </div>
+        <VeeForm class="space-y-6" @submit="handleSubmit">
+          <UsernameComponent v-model="data.username"/>
+          <PasswordFormComponent v-model="data.password" />
 
-          <div class="mt-1 sm:mt-0 sm:col-span-2">
-            <div class="input-frame">
-              <span class="input-label"> Password </span>
-              <input type="password" v-model="data.password" id="password" autocomplete="password" class="input-field" />
-            </div>
-          </div>
           <div class="button-container">
             <button type="submit" class="button-submit">Submit</button>
-            <button type="reset" class="button-reset" @click.prevent="handleClear">Clear</button>
+            <button type="reset" class="button-reset" @click="handleClear">Clear</button>
           </div>
-        </form>
+        </VeeForm>
       </div>
     </div>
   </div>
@@ -34,9 +24,17 @@
 
 <script>
 import {reactive} from "vue";
+import {Form} from 'vee-validate'
+import UsernameComponent from "@/components/formComponents/UsernameComponent";
+import PasswordFormComponent from "@/components/formComponents/PasswordFormComponent";
 
 export default {
   name: "LoginView",
+  components: {
+    VeeForm: Form,
+    UsernameComponent,
+    PasswordFormComponent,
+  },
   setup() {
     const data = reactive({
       username: '',
